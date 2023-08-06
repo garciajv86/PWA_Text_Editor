7f6123a02fd2434620c8c30ae48591c5738e8b2f
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2023-08-06 18:41:03 -0500

    corrected file path

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -42,7 +42,7 @@ module.exports = () => {
         background_color: "#225ca3",
         theme_color: "#225ca3",
         start_url: "./",
-        publicPath: "/",
+        publicPath: "./",
         icons: [
           {
             src: path.resolve("./src/images/logo.png"),
